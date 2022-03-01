56f5f51808a7e5cd8ad089c11db0f47fd88e281f
dkargilis <65100328+dkargilis@users.noreply.github.com>
2022-03-01 09:30:02 -0500

    Update config.js

--- config.js
+++ config.js
@@ -7,35 +7,24 @@ const ICONS = [
     icon: "ri-youtube-fill",
     link: "https://www.youtube.com/"
   },
-  {
-    icon: "ri-whatsapp-fill",
-    link: "https://web.whatsapp.com/"
-  },
-  {
-    icon: "ri-reddit-fill",
-    link: "https://www.reddit.com/"
-  },
-  {
-    icon: "ri-github-fill",
-    link: "https://www.github.com/"
-  }
+  
 ]
 
 const LIST_ITEMS = [
   {
-    name: "music",
-    link: "https://music.youtube.com"
+    name: "blackboard",
+    link: "https://blackboard.jhu.edu/webapps/login/"
   },
   {
-    name: "netflix",
-    link: "https://www.netflix.com/"
+    name: "pubmed",
+    link: "https://pubmed.ncbi.nlm.nih.gov/?otool=jhumlib"
   },
   {
-    name: "r/unixporn",
-    link: "https://www.reddit.com/r/unixporn/"
+    name: "embase",
+    link: "https://www.embase.com/?phase=continueToApp#search"
   },
   {
-    name: "r/firefoxcss",
-    link: "https://www.reddit.com/r/firefoxcss/"
+    name: "scholar",
+    link: "https://databases.library.jhu.edu/databases/proxy/JHU03174"
   },
 ]
